85cca3ab1f15a62501662e3012f217009df77aef
davisssamuel <davissamuelg1@gmail.com>
2024-06-05 10:25:49 -0400

    added verbose output for encryption

--- bincrypt.c
+++ bincrypt.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <math.h>
 
-int  ENCRYPT;
-int  KEY     = 0; 
+int  ENCRYPT = -1;
+int  KEY     = 0;
 char *INPUT  = NULL;
 char *OUTPUT = NULL;
 int  VERBOSE = 0;
@@ -15,10 +15,8 @@ void usage(char *program_name);
 int main(int argc, char **argv) {
     
   int opt;
-  while ((opt = getopt(argc, argv, "e:d:i:o:")) != -1) {
+  while ((opt = getopt(argc, argv, "e:d:i:o:v")) != -1) {
     switch (opt) {
-      case 'v':
-        VERBOSE = 1;
       case 'e':
         ENCRYPT = 1;
         KEY = atoi(optarg);
@@ -32,6 +30,9 @@ int main(int argc, char **argv) {
         break;
       case 'o':
         OUTPUT = optarg;
+        break;
+      case 'v':
+        VERBOSE = 1;
         break;
       default:
         usage(argv[0]);
@@ -62,9 +63,23 @@ int main(int argc, char **argv) {
       fread(buf, sizeof(buf), 1, infile);
       int ascii_code = (int)*buf + KEY + i;
       fwrite(&ascii_code, sizeof(ascii_code),1 , outfile);
+      if (VERBOSE == 1) {
+        int binary_num[32];
+        for (int j = 31; j >= 0; j--) {
+          if (ascii_code == 0) {
+            binary_num[j] = 0;
+          } 
+          else {
+            binary_num[j] = ascii_code % 2;
+            ascii_code /= 2;
+          }
+        }
+        for (int j = 0; j < 32; j++) {
+          printf("%d", binary_num[j]);
+        }
+      }
       i++;
     }
-
     fclose(outfile);
     fclose(infile);
   }
@@ -95,12 +110,11 @@ int main(int argc, char **argv) {
       }
       i++;
     }
-
     fclose(outfile);
     fclose(infile);
   } 
   else {
-    printf("Error: operation not specified\n");
+    printf("Error: cryptographic operation not specified\n");
     usage(argv[0]);
     return 1;
   }
@@ -108,5 +122,5 @@ int main(int argc, char **argv) {
 }
 
 void usage(char *program_name) {
-  printf("Usage: %s [-e] [key] [-i] [input_file] [-o] [output_file]\n", program_name);
+  printf("Usage: %s [-ed] [key] [-i] [input_file] [-o] [output_file] [-v]\n", program_name);
 }
