dd75c746e15130c2891a84c71e9630388b617f6b
davisssamuel <davissamuelg1@gmail.com>
2025-12-10 21:09:40 -0500

    feat: refactored source code and created new binary

--- main.c
+++ main.c
@@ -1,62 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int encrypt_file(FILE *ifile, FILE *ofile, const int key, const int verbose) {
-    char buf[1]; // buf is each character
-    int i = 0;
-
-    while (!feof(ifile)) {
-        fread(buf, sizeof(buf), 1, ifile);
-        int ascii_code = (int) *buf + key + i;
-        fwrite(&ascii_code, sizeof(ascii_code), 1, ofile);
-
-        // Print encryption output to the console
-        if (verbose == 1) {
-            int binary_num[32];
-
-            for (int j = 31; j >= 0; j--) {
-                if (ascii_code != 0) {
-                    binary_num[j] = ascii_code % 2;
-                    ascii_code /= 2;
-                } else {
-                    binary_num[j] = 0;
-                }
-            }
-
-            for (int j = 0; j < 32; j++) {
-                printf("%d", binary_num[j]);
-            }
-        }
-        i++;
+void print_binary(unsigned int n) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (n >> i) & 1);
     }
-
-    fclose(ifile);
-    fclose(ofile);
-
-    return 0;
+    printf("\n");
 }
 
-int decrypt_file(FILE *ifile, FILE *ofile, const int key, const int verbose) {
+void encrypt_file(FILE *ifile, FILE *ofile, const int key, const int verbose) {
+    unsigned char buf; // buf is a single character
     int i = 0;
-    int buf; // buf is each integer (32 bits)
 
-    while (!feof(ifile)) {
-        fread(&buf, sizeof(buf), 1, ifile);
-        buf -= key + i;
-
-        if (buf > 8) { // exclude ascii values 0-8
-            fprintf(ofile, "%c", (char) buf);
-
-            // Print decryption output to the console
-            if (verbose == 1) { printf("%c", (char) buf); }
-        }
+    while (fread(&buf, sizeof(buf), 1, ifile) == 1) {
+        int encrypted_char = buf + i + key;
+        fwrite(&encrypted_char, sizeof(encrypted_char), 1, ofile);
+        if (verbose == 1) { print_binary(encrypted_char); }
         i++;
     }
 
     fclose(ifile);
     fclose(ofile);
+}
 
-    return 0;
+void decrypt_file(FILE *ifile, FILE *ofile, const int key, const int verbose) {
+    int buf; // buf is 32 bits
+    int i = 0;
+
+    while (fread(&buf, sizeof(buf), 1, ifile) == 1) {
+        buf -= i + key;
+        fprintf(ofile, "%c", (char) buf);
+        if (verbose == 1) { printf("%c", (char) buf); }
+        i++;
+    }
+
+    fclose(ifile);
+    fclose(ofile);
 }
 
 void usage(char *program_name) {
@@ -101,7 +80,7 @@ int main(int argc, char *argv[]) {
     }
 
     FILE *ifile;
-    ifile = fopen(input_arg, "rb");
+    ifile = fopen(input_arg, "r");
     if (ifile == NULL) {
         printf("Error: file %s not found\n", input_arg);
         return 1;
@@ -115,12 +94,14 @@ int main(int argc, char *argv[]) {
     }
 
     if (encrypt_flag != 0) {
-        return encrypt_file(ifile, ofile, encrypt_flag, verbose_flag);
+        encrypt_file(ifile, ofile, encrypt_flag, verbose_flag);
     } else if (decrypt_flag != 0) {
-        return decrypt_file(ifile, ofile, decrypt_flag, verbose_flag);
+        decrypt_file(ifile, ofile, decrypt_flag, verbose_flag);
     } else {
         printf("Error: operation not specified\n");
         usage(argv[0]);
         return 1;
     }
+
+    return 0;
 }
